b3eaee29a9ecafdb567c64128d65fbfb6cbc915b
ashbits <ashutosh.luthra@gmail.com>
2019-03-14 12:57:25 +0530

    fix env vars

--- app/config/settings/settings-config.js
+++ app/config/settings/settings-config.js
@@ -16,9 +16,9 @@ function initializeSettings(settings) {
 function createArgumentSettings(settings) {
   settings.clusterEnabled = commandLineArgs[2] ? parseInt(commandLineArgs[2]) : 0;
   settings.environment = commandLineArgs[3] ? commandLineArgs[3].toLowerCase() : 'prod';
-  settings.hostName = commandLineArgs[4] ? commandLineArgs[4] : '127.0.0.1';
-  settings.masterPort =  commandLineArgs[5] ? parseInt(commandLineArgs[5]) : 3000;
-  settings.workerPort =  commandLineArgs[6] ? parseInt(commandLineArgs[6]) : 9000;
+  settings.hostName = process.env.hostName || '127.0.0.1';
+  settings.masterPort =  process.env.PORT || 3000;
+  settings.workerPort =  process.env.PORT || 9000;
 }
 
 function loadConfigSettings(settings) {
